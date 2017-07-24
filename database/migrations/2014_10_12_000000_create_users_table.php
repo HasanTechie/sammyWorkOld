4f1f2cde1aa6a09cc4323a7eac8ed2c6923367fa
hasanabbax <hasanabbax@gmail.com>
2017-07-24 16:22:52 +0000

    added User, EducationalVerification fields

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,22 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            //`u_uid`, `u_organization_name`, `u_first_name`, `u_last_name`, `u_contact`, `u_country`, `u_state`, `u_city`, `u_is_company`, `u_email`, `u_password`, `u_bio`, `u_url_of_photo_image`, `u_url_of_nic_image`, `u_date_registered`, `u_hash`, `u_is_email_verified`, `u_is_active`
             $table->increments('id');
-            $table->string('name');
+            $table->string('organization_name');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('contact');
+            $table->string('state');
+            $table->string('city');
             $table->string('email')->unique();
             $table->string('password');
+            $table->text('bio');
+            $table->string('photo');
+            $table->string('identitydocument');
+            $table->tinyInteger('is_company');
+            $table->tinyInteger('is_email_verified');
+            $table->tinyInteger('is_active');
             $table->rememberToken();
             $table->timestamps();
         });
--- database/migrations/2017_07_24_161516_create_educational_verifications_table.php
+++ database/migrations/2017_07_24_161516_create_educational_verifications_table.php
@@ -14,7 +14,30 @@ class CreateEducationalVerificationsTable extends Migration
     public function up()
     {
         Schema::create('educational_verifications', function (Blueprint $table) {
-            $table->increments('id');
+            //`ud_uid`, `ud_type`, `ud_surname`, `ud_first_name`, `ud_middle_name`, `ud_date_of_birth`, `ud_date_created`, `ud_created_by_u_uid`, `ud_response_file`, `ud_response_message`, `ud_is_verified`, `ud_is_verified_by_uid`, `ud_is_verified_by_institute`, `ud_transaction_serialize_array`, `ud_is_payment_success` and `ud_uid`, `ev_pi_name`, `ev_pi_id`, `ev_year_of_graduation`, `ev_matric_number`, `ev_course_offered`, `ev_degree_obtained`, `ev_degree_class`, `ev_purpose_of_verification`, `ev_url_of_document`, `ev_url_of_academic_degree`
+            $table->uuid('id');
+            $table->string('type');
+            $table->string('surname');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->date('date_of_birth');
+            $table->string('response_file');
+            $table->text('response_message');
+            $table->tinyInteger('is_verified');
+            $table->string('verified_by_id');
+            $table->tinyInteger('is_verified_by_institute');
+            $table->string('transaction_serialize_array');
+            $table->tinyInteger('is_payment_succeed');
+            $table->string('institute_name');
+            $table->string('institute_id');
+            $table->integer('year_of_graduation');
+            $table->string('matric_number');
+            $table->string('course_offered');
+            $table->string('degree_obtained');
+            $table->string('degree_class');
+            $table->text('purpose_of_verification');
+            $table->string('document');
+            $table->string('academic_degree');
             $table->timestamps();
         });
     }
